0274970bbfc29347f55ef6363cf94d5c2b94300a
Luis Armendariz <luis.armendariz@gmail.com>
2024-08-18 21:21:50 -0700

    Add formula for pan-utils

---
+++ Formula/pan-utils.rb
@@ -0,0 +1,20 @@
+class PanUtils < Formula
+  desc "Miscellaneous Utilities"
+  homepage "https://github.com/ludwig/pan-utils.git"
+  #url "https://github.com/ludwig/pan-utils/archive/v0.1.0.tar.gz"
+  #sha256 "b1b3b3b3b3b3b3b3b3b3b3b3b3b3b3b3b3b3b3b3b3b3b3b3b3b3b3b3b3"
+  head "git@yappa:scratch/pan-utils.git"
+  license "null"
+
+  def install
+    bin.install Dir["bin/*"]
+  end
+
+  def caveats
+    <<~EOS
+      For now, this formula only supports installation from the HEAD branch.
+      Please use the following command to install:
+        brew install --HEAD pan-utils
+    EOS
+  end
+end
